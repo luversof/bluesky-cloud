892bad19461466210a5686c18009ad4c0f5ece1d
bluesky <bluesky@bluesky2.korea.ncsoft.corp>
2018-02-28 17:21:49 +0900

    cloud  설정 boot 쪽으로 위임 (임시..)

--- bluesky-cloud-parent/bluesky-cloud-netflix-hystrix-dashboard/src/main/java/net/luversof/cloud/netflix/hystrix/dashboard/Application.java
+++ bluesky-cloud-parent/bluesky-cloud-netflix-hystrix-dashboard/src/main/java/net/luversof/cloud/netflix/hystrix/dashboard/Application.java
@@ -3,12 +3,10 @@ package net.luversof.cloud.netflix.hystrix.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableHystrixDashboard
 @EnableDiscoveryClient
-@FeignClient(value = "hystrix-dashboard")
 @SpringBootApplication
 public class Application {
 
