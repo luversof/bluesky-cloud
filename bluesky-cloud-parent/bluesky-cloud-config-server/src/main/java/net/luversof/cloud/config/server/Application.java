f62259d9891a191d024084d1ca14ec3e6210f76d
luver <luver@DESKTOP-VHHSBOB>
2019-02-24 09:27:40 +0900

    config server "/" 호출 처리 추가

--- bluesky-cloud-parent/bluesky-cloud-config-server/src/main/java/net/luversof/cloud/config/server/Application.java
+++ bluesky-cloud-parent/bluesky-cloud-config-server/src/main/java/net/luversof/cloud/config/server/Application.java
@@ -4,11 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
+	
+	@RestController
+	public static class IndexController {
+		@GetMapping("")
+		public String index() {
+			return "configServer";
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
