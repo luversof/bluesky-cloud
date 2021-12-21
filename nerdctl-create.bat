b7bc66206c8d4076da48a6bcc3c08eca37353612
luversof <luversof@hanmail.net>
2021-12-21 14:53:32 +0900

    local nerdctl create 스크립트 관련 수정
    github workflow java 11 ->17 변경

--- nerdctl-create.bat
+++ nerdctl-create.bat
@@ -1,23 +1,12 @@
-mvn -B -U -f ./bluesky-cloud-parent/pom.xml -DskipTests=true clean package
-
-nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-netflix-eureka-server/target/bluesky-cloud-netflix-eureka-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-netflix-eureka-server .
-
-nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-config-server/target/bluesky-cloud-config-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-config-server .
-
-nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-admin-server/target/bluesky-cloud-admin-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-admin-server .
-
-nerdctl tag luversof/bluesky-cloud-netflix-eureka-server:latest localhost:8083/luversof/bluesky-cloud-netflix-eureka-server
-
-nerdctl push localhost:8083/luversof/bluesky-cloud-netflix-eureka-server
-
-nerdctl tag luversof/bluesky-cloud-admin-server:latest localhost:8083/luversof/bluesky-cloud-admin-server
-
-nerdctl push localhost:8083/luversof/bluesky-cloud-admin-server
-
-nerdctl tag luversof/bluesky-cloud-config-server:latest localhost:8083/luversof/bluesky-cloud-config-server
-
-nerdctl push localhost:8083/luversof/bluesky-cloud-config-server
-
-kubectl delete -f kubernetes-bluesky-cloud-netflix-eureka-server.yaml -f kubernetes-bluesky-cloud-config-server.yaml -f kubernetes-bluesky-cloud-admin-server.yaml
-
-kubectl apply -f kubernetes-bluesky-cloud-netflix-eureka-server.yaml -f kubernetes-bluesky-cloud-config-server.yaml -f kubernetes-bluesky-cloud-admin-server.yaml
+call mvn clean package -B -U -f ./bluesky-cloud-parent/pom.xml -DskipTests=true -P nexus-local
+call nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-netflix-eureka-server/target/bluesky-cloud-netflix-eureka-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-netflix-eureka-server .
+call nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-config-server/target/bluesky-cloud-config-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-config-server .
+call nerdctl build --build-arg JAR_FILE=bluesky-cloud-parent/bluesky-cloud-admin-server/target/bluesky-cloud-admin-server-0.0.1-SNAPSHOT.jar --tag luversof/bluesky-cloud-admin-server .
+call nerdctl tag luversof/bluesky-cloud-netflix-eureka-server:latest localhost:8083/luversof/bluesky-cloud-netflix-eureka-server
+call nerdctl push localhost:8083/luversof/bluesky-cloud-netflix-eureka-server
+call nerdctl tag luversof/bluesky-cloud-admin-server:latest localhost:8083/luversof/bluesky-cloud-admin-server
+call nerdctl push localhost:8083/luversof/bluesky-cloud-admin-server
+call nerdctl tag luversof/bluesky-cloud-config-server:latest localhost:8083/luversof/bluesky-cloud-config-server
+call nerdctl push localhost:8083/luversof/bluesky-cloud-config-server
+call kubectl delete -f kubernetes-bluesky-cloud-netflix-eureka-server.yaml -f kubernetes-bluesky-cloud-config-server.yaml -f kubernetes-bluesky-cloud-admin-server.yaml
+call kubectl apply -f kubernetes-bluesky-cloud-netflix-eureka-server.yaml -f kubernetes-bluesky-cloud-config-server.yaml -f kubernetes-bluesky-cloud-admin-server.yaml
