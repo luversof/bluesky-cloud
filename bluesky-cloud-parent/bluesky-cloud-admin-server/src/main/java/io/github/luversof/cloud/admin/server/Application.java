76dd48e77c102b0d9862306e2316f8dd1e9d3910
bluesky <bluesky@ncsoft.com>
2023-01-05 13:50:08 +0900

    admin server @EnableScheduling 설정 추가

--- bluesky-cloud-parent/bluesky-cloud-admin-server/src/main/java/io/github/luversof/cloud/admin/server/Application.java
+++ bluesky-cloud-parent/bluesky-cloud-admin-server/src/main/java/io/github/luversof/cloud/admin/server/Application.java
@@ -3,12 +3,14 @@ package io.github.luversof.cloud.admin.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableScheduling
 public class Application {
 	
 	public static void main(String[] args) {
