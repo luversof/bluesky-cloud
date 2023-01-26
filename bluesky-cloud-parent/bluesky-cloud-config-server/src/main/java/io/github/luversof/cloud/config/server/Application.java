9ab2df32b0d28bd0f75c95856b82733cbb185e47
bluesky <bluesky@ncsoft.com>
2023-01-26 09:46:58 +0900

    @EnableDiscoveryClient 제거 (불필요 설정)

--- bluesky-cloud-parent/bluesky-cloud-config-server/src/main/java/io/github/luversof/cloud/config/server/Application.java
+++ bluesky-cloud-parent/bluesky-cloud-config-server/src/main/java/io/github/luversof/cloud/config/server/Application.java
@@ -2,13 +2,11 @@ package io.github.luversof.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 	
--- bluesky-cloud-parent/bluesky-cloud-gateway/src/main/java/io/github/luversof/cloud/gateway/Application.java
+++ bluesky-cloud-parent/bluesky-cloud-gateway/src/main/java/io/github/luversof/cloud/gateway/Application.java
@@ -2,11 +2,7 @@ package io.github.luversof.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 	
